32ab81e0fe20d787edbc45b52d4648b5ab30ca5a
atharva_dawande <atharva.dawande603@gmail.com>
2020-05-18 01:25:52 +0530

    all colors aligned with sound/music

--- lib/main.dart
+++ lib/main.dart
@@ -10,16 +10,66 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-
-              child: FlatButton(
+          child: Column(
+            children: <Widget>[
+              // onpressedinfo(1),
+              FlatButton(
                 onPressed: () {
                   final player = AudioCache();
                   player.play('note1.wav');
                 },
                 child: Text('click'),
+                color: Colors.red,
               ),
-
+              FlatButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note2.wav');
+                },
+                child: Text('click'),
+                color: Colors.blue,
+              ),
+              FlatButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note3.wav');
+                },
+                child: Text('click'),
+                color: Colors.green,
+              ),
+              FlatButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note4.wav');
+                },
+                child: Text('click'),
+                color: Colors.yellow,
+              ),
+              FlatButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note5.wav');
+                },
+                child: Text('click'),
+                color: Colors.orange,
+              ),
+              FlatButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note6.wav');
+                },
+                child: Text('click'),
+                color: Colors.blueAccent[300],
+              ),
+              FlatButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note7.wav');
+                },
+                child: Text('click'),
+                color: Colors.blueGrey[200],
+              ),
+            ],
           ),
         ),
       ),
