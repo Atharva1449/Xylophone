722a770ae6d04e995e92a4d67c05eea9c05a8f24
atharva_dawande <atharva.dawande603@gmail.com>
2020-05-18 17:03:05 +0530

    function driven code

--- lib/main.dart
+++ lib/main.dart
@@ -5,70 +5,107 @@ import 'package:flutter/material.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  Widget funct(int number, Color color) {
+    return Expanded(
+      child: FlatButton(
+        onPressed: () {
+          final player = AudioCache();
+          player.play('note$number.wav');
+        },
+        //child: Text('click'),
+        color: color,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
+              funct(1, Colors.red),
+              funct(2, Colors.blue),
+              funct(3, Colors.green),
+              funct(4, Colors.yellow),
+              funct(5, Colors.orange),
+              funct(6, Colors.indigo),
+              funct(7, Colors.lightBlueAccent),
+
               // onpressedinfo(1),
-              FlatButton(
-                onPressed: () {
-                  final player = AudioCache();
-                  player.play('note1.wav');
-                },
-                child: Text('click'),
-                color: Colors.red,
-              ),
-              FlatButton(
-                onPressed: () {
-                  final player = AudioCache();
-                  player.play('note2.wav');
-                },
-                child: Text('click'),
-                color: Colors.blue,
-              ),
-              FlatButton(
-                onPressed: () {
-                  final player = AudioCache();
-                  player.play('note3.wav');
-                },
-                child: Text('click'),
-                color: Colors.green,
-              ),
-              FlatButton(
-                onPressed: () {
-                  final player = AudioCache();
-                  player.play('note4.wav');
-                },
-                child: Text('click'),
-                color: Colors.yellow,
-              ),
-              FlatButton(
-                onPressed: () {
-                  final player = AudioCache();
-                  player.play('note5.wav');
-                },
-                child: Text('click'),
-                color: Colors.orange,
-              ),
-              FlatButton(
-                onPressed: () {
-                  final player = AudioCache();
-                  player.play('note6.wav');
-                },
-                child: Text('click'),
-                color: Colors.blueAccent[300],
-              ),
-              FlatButton(
-                onPressed: () {
-                  final player = AudioCache();
-                  player.play('note7.wav');
-                },
-                child: Text('click'),
-                color: Colors.blueGrey[200],
-              ),
+//              Expanded(
+//                child: FlatButton(
+//                  onPressed: () {
+//                    final player = AudioCache();
+//                    player.play('note1.wav');
+//                  },
+//                  //child: Text('click'),
+//                  color: Colors.red,
+//                ),
+//              ),
+//              Expanded(
+//                child: FlatButton(
+//                  onPressed: () {
+//                    final player = AudioCache();
+//                    player.play('note2.wav');
+//                  },
+//                  //child: Text('click'),
+//                  color: Colors.blue,
+//                ),
+//              ),
+//              Expanded(
+//                child: FlatButton(
+//                  onPressed: () {
+//                    final player = AudioCache();
+//                    player.play('note3.wav');
+//                  },
+//                  //child: Text('click'),
+//                  color: Colors.green,
+//                ),
+//              ),
+//              Expanded(
+//                child: FlatButton(
+//                  onPressed: () {
+//                    final player = AudioCache();
+//                    player.play('note4.wav');
+//                  },
+//                  //child: Text('click'),
+//                  color: Colors.yellow,
+//                ),
+//              ),
+//              Expanded(
+//                child: FlatButton(
+//                  onPressed: () {
+//                    final player = AudioCache();
+//                    player.play('note5.wav');
+//                  },
+//                  //child: Text('click'),
+//                  color: Colors.orange,
+//                ),
+//              ),
+//              Expanded(
+//                child: FlatButton(
+//                  onPressed: () {
+//                    final player = AudioCache();
+//                    player.play('note6.wav');
+//                  },
+//                  //child: Text('click'),
+//                  color: Colors.blue,
+//                ),
+//              ),
+//              Expanded(
+//                child: FlatButton(
+//                  onPressed: () {
+//                    final player = AudioCache();
+//                    player.play('note7.wav');
+//                  },
+//                  //child: Text('click'),
+//                  color: Colors.blueGrey[200],
+//                ),
+//              ),
             ],
           ),
         ),
